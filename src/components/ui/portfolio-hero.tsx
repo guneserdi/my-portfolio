3e8bc0251578c6767b96409ddef9393c71c25ec7
guneserdi <guneserdi@gmail.com>
2026-01-21 00:19:31 +0300

    logo21.21

--- src/components/ui/portfolio-hero.tsx
+++ src/components/ui/portfolio-hero.tsx
@@ -311,7 +311,7 @@ export default function PortfolioHero() {
   <div className="absolute top-6 left-1/2 -translate-x-1/2 z-20 flex flex-col items-center gap-1">
     {/* Mandalina/Logo İkonu */}
     <img 
-      src="/meldalogo1.png" 
+      src="/meldalogo.svg" 
       alt="Logo" 
       className="h-10 w-auto object-contain" 
     />
